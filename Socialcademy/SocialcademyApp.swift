072b0a74b109f38a91a27c5d250a4c58a64aa6ec
iiiivaska <iiiivaska@yandex.ru>
2022-03-06 15:07:18 +0300

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Василий Буланов on 06.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
